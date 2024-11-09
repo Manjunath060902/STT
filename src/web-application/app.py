1fe036212c83b735ddbe8253947ecd729d6aed82
Manjunath060902 <245120733109@mvsrec.edu.in>
2024-11-09 11:34:22 +0530

    Update app.py

--- src/web-application/app.py
+++ src/web-application/app.py
@@ -52,13 +52,13 @@ def download_public_key(username):
 			list = file.split('-')
 			if list[0] == username:
 				filename = UPLOAD_KEY+file
-				return send_file(filename, attachment_filename='publicKey.pem',as_attachment=True)
+				return send_file(filename, download_name='publicKey.pem',as_attachment=True)
 
 @app.route('/file-directory/retrieve/file/<filename>')
 def download_file(filename):
 	filepath = UPLOAD_FOLDER+filename
 	if(os.path.isfile(filepath)):
-		return send_file(filepath, attachment_filename='fileMessage-thrainSecurity.txt',as_attachment=True)
+		return send_file(filepath, download_name='fileMessage-thrainSecurity.txt',as_attachment=True)
 	else:
 		return render_template('file-list.html',msg='An issue encountered, our team is working on that')
 
