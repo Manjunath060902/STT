bfca40e4b75af5990c89f8d27da93430b3281bab
Manjunath060902 <245120733109@mvsrec.edu.in>
2025-03-08 00:33:58 +0530

    Update app.py

--- src/web-application/app.py
+++ src/web-application/app.py
@@ -169,3 +169,13 @@ def register_user():
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=80)
 	# app.run(debug=True)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('demo2.html')  # Render the demo2.html template
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the Flask app
